2b5654a27e0e7788e6c2e417a093da9c9af42087
Shahin M <s.mammadov@jacobs-university.de>
2022-08-30 20:23:24 +0400

    jupyter cs bs4

--- Python/Week 4/W4-D1-CS-BS4.py
+++ Python/Week 4/W4-D1-CS-BS4.py
@@ -1,4 +1,3 @@
-from turtle import pos
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,15 +14,23 @@ votes_each = soup.find_all('div', attrs = {'class':'_1rZYMD_4xY3gRcSS3p8ODO _3a2
 dates_each = soup.find_all('span', attrs = {'class':'_2VF2J19pUIMSLJFky-7PEI'})
 links_each = soup.find_all('a', attrs = {'class':'SQnoC3ObvgnGjWt90zD9Z _2INHSNB8V5eaWp4P0rY_mE'})
 
-""" for row in posts_each:
-    print(row.get_text()) """
 
-""" for row in votes_each:
-    print(row.get_text()) """
+posts = []
+votes = []
+dates = []
+links = []
 
-""" for row in dates_each:
-    print(row.get_text()) """
+for row in posts_each:
+    posts.append(row.get_text()) 
+
+for row in votes_each:
+    votes.append(row.get_text()) 
+
+for row in dates_each:
+    dates.append(row.get_text()) 
 
 for row in links_each:
-    print(row['href'])
+    links.append("https://www.reddit.com/" + row['href'])
+
+
 
